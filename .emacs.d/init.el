0da5826068645ce1b0a7198ce064ff9b7f74ce4f
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2020-03-30 15:28:04 +0300

    emacs: asm-mode _

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -211,6 +211,8 @@ the name of FILE in the current directory, suitable for creation"
 (modify-syntax-entry ?_ "w" c++-mode-syntax-table)
 (modify-syntax-entry ?_ "w" makefile-mode-syntax-table)
 (modify-syntax-entry ?_ "w" python-mode-syntax-table)
+(with-eval-after-load 'asm-mode
+  modify-syntax-entry ?_ "w" asm-mode-syntax-table)
 (with-eval-after-load 'org
   (modify-syntax-entry ?_ "w" org-mode-syntax-table))
 
