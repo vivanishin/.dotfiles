96ca2a69960043f84ceadc51b450dfd303dd071c
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2023-02-23 21:32:59 +0300

    emacs: nasm-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -85,6 +85,9 @@
 (use-package python-mode
   :ensure t)
 
+(use-package nasm-mode
+  :ensure t)
+
 (use-package ido
   :ensure t
   :config
@@ -309,7 +312,7 @@ the name of FILE in the current directory, suitable for creation"
      ("gnu" . "http://elpa.gnu.org/packages/")
      ("org" . "http://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(which-key projectile dired cquery auto-package-update flycheck lsp-mode ggtags wc-mode default-text-scale python-info bbdb grep-a-lot lispy dired-x paredit evil-paredit image+ key-chord evil-search-highlight-persist highlight evil-leader pdf-tools magit use-package solarized-theme evil))
+   '(nasm-mode which-key projectile dired cquery auto-package-update flycheck lsp-mode ggtags wc-mode default-text-scale python-info bbdb grep-a-lot lispy dired-x paredit evil-paredit image+ key-chord evil-search-highlight-persist highlight evil-leader pdf-tools magit use-package solarized-theme evil))
  '(scheme-program-name "guile")
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
