91038c96e471d7cd082c4ba5b859eb0f369ea667
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2023-10-09 09:20:25 +0300

    emacs: yaml-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -149,6 +149,10 @@
   :config
   (global-undo-tree-mode))
 
+(use-package yaml-mode
+  :mode "\\.yml\\'"
+  :ensure t)
+
 (use-package evil
   :ensure t
 
@@ -316,7 +320,7 @@ the name of FILE in the current directory, suitable for creation"
      ("gnu" . "http://elpa.gnu.org/packages/")
      ("org" . "http://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(nasm-mode which-key projectile dired cquery auto-package-update flycheck lsp-mode ggtags wc-mode default-text-scale python-info bbdb grep-a-lot lispy dired-x paredit evil-paredit image+ evil-search-highlight-persist highlight evil-leader pdf-tools magit use-package solarized-theme evil))
+   '(yaml-mode nasm-mode which-key projectile dired cquery auto-package-update flycheck lsp-mode ggtags wc-mode default-text-scale python-info bbdb grep-a-lot lispy dired-x paredit evil-paredit image+ evil-search-highlight-persist highlight evil-leader pdf-tools magit use-package solarized-theme evil))
  '(scheme-program-name "guile")
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
