f1c93fa4f0d249f5168296bfd3ebd569a28c598c
Vladislav Ivanishin <vlad@ispras.ru>
2023-10-20 16:59:27 +0300

    qutebrowser: upgrade adaptation: content.javascript.clipboard

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -589,7 +589,7 @@ c.content.blocking.hosts.lists = [
 ## QtWebEngine, writing the clipboard as response to a user interaction
 ## is always allowed.
 ## Type: Bool
-c.content.javascript.can_access_clipboard = True
+c.content.javascript.clipboard = "access"
 
 ## Allow JavaScript to close tabs.
 ## Type: Bool
